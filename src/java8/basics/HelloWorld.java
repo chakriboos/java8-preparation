ae9d39faa4cb0fbf427824b0a3829bb46fdeda04
chakradhara reddy <chakri.boost@gmail.com>
2016-07-07 15:42:09 +0530

    initial commit for in-home

---
+++ src/java8/basics/HelloWorld.java
@@ -0,0 +1,13 @@
+package java8.basics;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		int d =0;
+		int a = 10/d;
+
+	}
+
+}
