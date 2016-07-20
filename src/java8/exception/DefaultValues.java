cd1608443941ed0339d9e97a1e1742bbb1b33e5d
ChakradharaReddy Dwarasala <ChakradharaReddy.Dwarasala@capitalone.com>
2016-07-20 11:43:59 +0530

    some more examples added

---
+++ src/java8/exception/DefaultValues.java
@@ -0,0 +1,14 @@
+package java8.exception;
+
+public class DefaultValues {
+
+		byte b;
+		short s;
+		int i;
+		long l;
+		float f;
+		double d;
+		char c;
+		Parent1 p1;
+
+}
---
+++ src/java8/exception/DefaultValuesDemo.java
@@ -0,0 +1,18 @@
+package java8.exception;
+
+public class DefaultValuesDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DefaultValues dv = new DefaultValues();
+		System.out.println(dv.b);
+		System.out.println(dv.s);
+		System.out.println(dv.i);
+		System.out.println(dv.l);
+		System.out.println(dv.f);
+		System.out.println(dv.d);
+		System.out.println(dv.c);
+		System.out.println(dv.p1);
+	}
+
+}
